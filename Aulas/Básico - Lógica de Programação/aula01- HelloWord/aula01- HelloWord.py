30c94d93c4c62a2980c596b9610e6c9bc7c5aa32
cicerooficial <cicerooficial@gmail.com>
2021-03-18 22:38:29 -0300

    Update title file aulas 01,02,03,04,05,06,07,08 e 09. Update de title files exercícios 01,02 e 03

---
+++ Aulas/Básico - Lógica de Programação/aula01- HelloWord/aula01- HelloWord.py
@@ -0,0 +1,5 @@
+# Cerquilha para comentário de linha única #Padrão
+"""
+3 Aspas duplas ou simples para documentação/comentários longos
+"""
+print('Hello Word!')  # Sempre deixar 2 espaços para este tipo de comentário
