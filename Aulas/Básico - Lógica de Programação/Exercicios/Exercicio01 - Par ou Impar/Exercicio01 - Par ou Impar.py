6e80881fd56367954ece707ee80dbc72fb54000a
cicerooficial <cicerooficial@gmail.com>
2021-03-18 22:55:23 -0300

    Update Exercicio01 - Par ou Impar.py

--- Aulas/Básico - Lógica de Programação/Exercicios/Exercicio01 - Par ou Impar/Exercicio01 - Par ou Impar.py
+++ Aulas/Básico - Lógica de Programação/Exercicios/Exercicio01 - Par ou Impar/Exercicio01 - Par ou Impar.py
@@ -1,5 +1,18 @@
 """
 Faça um programa que peça ao usuário para digitar um número inteiro,
-informe se este número é par ou ímpar. Caso o usuário não digite um número inteiro,
+informe se este número é par ou ímpar.
+Caso o usuário não digite um número inteiro,
 informe que não é um número inteiro.
 """
+
+numero = input('Digite um numero: ')
+
+if numero.isnumeric():
+    numero = int(numero)
+    resultado = numero % 2
+    if resultado != 0:
+        print(f'O numero {numero} é Impar!')
+    else:
+        print(f'O numero {numero} é Par!')
+else:
+    print('Não foi informado um número inteiro. Tente novamente!')
