7f22617955842920a1a43adc675fb1e961b25e57
cicerooficial <cicerooficial@gmail.com>
2021-03-18 22:54:50 -0300

    delete files aulas 1,2,3,4,5,6,7,8 and 9. Delete files exercicio 1,2 anda 3

--- Aulas/Básico - Lógica de Programação/Exercicios/Exercicio3 - Contanto Caracter/Exercicio3 - Contanto Caracter.py
+++
@@ -1,6 +0,0 @@
-"""
-Faça um programa que peça o primeiro nome do usuário.
-Se o nometiver 4 letras ou menos escreva "Seu nome é curto";
-Se tiver entre 5 e 6 letras, escreva "Seu nome é normal";
-Se for maior que 6 letras, escreva "Seu nome é muito grande".
-"""
--- Aulas/Básico - Lógica de Programação/aula1- HelloWord/aula1_helloWord.py
+++
@@ -1,5 +0,0 @@
-# Cerquilha para comentário de linha única #Padrão
-"""
-3 Aspas duplas ou simples para documentação/comentários longos
-"""
-print('Hello Word!')  # Sempre deixar 2 espaços para este tipo de comentário
